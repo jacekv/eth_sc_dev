0578064987d9cf3f76c1ad95c7a385813724170e
Jacek Varky <jacek.varky@maibornwolff.de>
2018-10-01 21:11:50 +0200

    Added another exception to the stack exception class. Commented to stack.py file

--- eth/CallStackExceptions.py
+++ eth/CallStackExceptions.py
@@ -2,3 +2,9 @@ class CallStackDepthReached(Exception):
     def __init__(self, message):
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
+
+
+class CallStackValueToLarge(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
