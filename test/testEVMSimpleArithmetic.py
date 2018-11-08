aee1b515c10ff7cb069c6d287967c127f59dd8d5
Jacek Varky <jacek.varky@maibornwolff.de>
2018-11-08 19:15:06 +0100

    Test for simple arithmetic

--- test/testEVMSimpleArithmetic.py
+++ test/testEVMSimpleArithmetic.py
@@ -29,11 +29,24 @@ class EVMArithmeticTest(unittest.TestCase):
         self.evm.executeCode()
         self.assertEqual(int(self.evm.stack.pop().getWord(), 16), 0x3377)
 
+    def testMultiplication(self):
+        code = '603361334402'
+        self.evm.setCode(code)
+        self.evm.executeCode()
+        self.assertEqual(int(self.evm.stack.pop().getWord(), 16), 0xA368C)
+
     def testSubstraction(self):
         code = '603361334403'
         self.evm.setCode(code)
         self.evm.executeCode()
         self.assertEqual(int(self.evm.stack.pop().getWord(), 16), 0x3311)
+
+    def testDivisionNonZero(self):
+        code = '603361334505'
+        self.evm.setCode(code)
+        self.evm.executeCode()
+        self.assertEqual(int(self.evm.stack.pop().getWord(), 16), 0x101)
+
 
     def runTest(self):
         methods = dir(self)
